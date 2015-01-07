cf51642aca3148a60b597a3031992ba03ff7f5c1
SuchForbiddenColors <stueck4507@gmail.com>
2015-01-07 15:20:16 -0600

    Newest version of defines

---
+++ Defines.h
@@ -0,0 +1,15 @@
+#define MOTOR_DRIVE_LEFT 1
+#define MOTOR_DRIVE_RIGHT 2
+
+#define XSTICK_PORT 1
+
+#define X_A 1
+#define X_B 2
+#define X_X 3
+#define X_Y 4
+#define X_LEFT_BUMPER 5
+#define X_RIGHT_BUMPER 6
+#define X_BACK 7
+#define X_START 8
+#define X_LEFT_CLICK 9
+#define X_RIGHT_CLICK 10
