e86082e0c6ab88781cdc6113ea88674e0e902fee
SuchForbiddenColors <stueck4507@gmail.com>
2015-01-13 20:08:06 -0600

    Defines with some encoder stuff
    Pulses per revolution was hard-won. I had to manually turn a wheel with tape on it.

--- Defines.h
+++ Defines.h
@@ -1,7 +1,13 @@
-#define MOTOR_DRIVE_LEFT 1
-#define MOTOR_DRIVE_RIGHT 2
+#define MOTOR_DRIVE_LEFT 0
+#define MOTOR_DRIVE_RIGHT 1
+#define TEST_MOTOR_CHANNEL 2
 
 #define XSTICK_PORT 1
+#define ENCODER_CHANNEL_A 1
+#define ENCODER_CHANNEL_B 2
+
+#define TURN_INVERT -1
+#define ENCODER_ONE_PULSES_PER_REVOLUTION 245
 
 #define X_A 1
 #define X_B 2
@@ -13,3 +19,4 @@
 #define X_START 8
 #define X_LEFT_CLICK 9
 #define X_RIGHT_CLICK 10
+
