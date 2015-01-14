46db264c23511ce6fa4358623f92a632f0086edc
SuchForbiddenColors <stueck4507@gmail.com>
2015-01-13 20:16:47 -0600

    .h Code to do some encodilation.

--- Wheelz.h
+++ Wheelz.h
@@ -4,14 +4,22 @@ class Wheelz
 {
 protected:
 	RobotDrive* wheels;
-
+	Encoder *encoder;
 
 
 public:
-	Wheelz(int leftMotor, int rightMotor);
+	Wheelz(int leftMotor, int rightMotor, int aChannel, int bChannel);
+
 	void XDrive(GenericHID * XStick);
+	void CarefulDrive(GenericHID * XStick);
 	void DissectedDrive(float forward, float turn);
+
+	float GetEncoder();
+	bool GetDirectionEncoder();
+	void EncoderTurn(float rotations, Victor *testMotor);
+
 	void InitWatchdog(bool is);
 	void SetExpiration(float exp);
 
 };
+
