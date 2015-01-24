2b23b0a1041c8c0938241d5c16a3ee0be04c0e54
SuchForbiddenColors <stueck4507@gmail.com>
2015-01-24 12:58:22 -0600

    Also there's a GetString function

--- Dash.h
+++ Dash.h
@@ -1,6 +1,8 @@
 #include "WPILib.h"
 #include "Wheelz.h"
 #include "Pneumatics.h"
+#include "Vision.h"
+#include "Defines.h"
 
 
 using namespace std;
@@ -9,15 +11,19 @@ class Dash {
 protected:
 	Wheelz *whe;
 	Pneumatics *pne;
-
+	BuiltInAccelerometer *ace;
 
 
 public:
-	Dash(Wheelz *wheels, Pneumatics *air);
+	Dash(Wheelz *wheels, Pneumatics *air, BuiltInAccelerometer *excel);
 	void PutString(int lineNum, string message);
 	void PutNumber(int sliderNum, float number);
 
+	string GetString(int lineNum); //Returns the string at a line of the dashboard
+	float GetNumber(int sliderNum); //Returns the number at a slider of the dashboard
+
+	void EncoderCount(int sliderNum); //Writes the percentage of encoder rotations on a slider on the dashboard
+	void Acceleration(int sliderNum, int axis);
 
 
 };
-
